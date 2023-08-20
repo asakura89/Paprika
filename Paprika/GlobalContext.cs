56c5e50747a24e88b989defef958331dcbcce66d
asakura89 <flamboyan3no1a@gmail.com>
2023-08-20 12:14:13 +0700

    [New] Upgrade to .NET 7
    [Fixed] Add Paprika tasks folder by config

--- Paprika/GlobalContext.cs
+++ Paprika/GlobalContext.cs
@@ -1,6 +1,10 @@
+using System.Xml;
+using AppSea;
+using Eksmaru;
 using Emi;
 using Reflx;
 using Ria;
+using Varya;
 
 namespace Paprika;
 
@@ -10,20 +14,48 @@ public static class GlobalContext {
     public static IPipelineExecutor PipelineExecutor { get; set; }
 
     public static void Initialize() {
-        DynamicLoadAssemblies();
+        String paprikaConfigPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "paprika.config.xml");
+        IList<String> locations = GetAssemblyLocations(paprikaConfigPath);
+        DynamicLoadAssemblies(locations);
 
         ITypeAndAssemblyParser typeNAsmParser = TypeAndAssemblyParser.Instance;
-        String paprikaConfig = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "paprika.config.xml");
-        Emitter = new XmlConfigEmitterLoader(typeNAsmParser, paprikaConfig).Load();
-        PipelineExecutor = new XmlConfigPipelineLoader(paprikaConfig).Load();
+        Emitter = new XmlConfigEmitterLoader(typeNAsmParser, paprikaConfigPath).Load();
+        PipelineExecutor = new XmlConfigPipelineLoader(paprikaConfigPath).Load();
     }
 
-    static void DynamicLoadAssemblies() {
+    static void DynamicLoadAssemblies(IList<String> locations) {
         IDefaultAssemblyResolver asmResolver = new DefaultAssemblyResolver();
         AppDomain.CurrentDomain.AssemblyResolve += asmResolver.Resolve;
 
         IAssemblyLoader asmLoader = new AssemblyLoader();
         asmLoader.LoadFromPath(AppDomain.CurrentDomain.BaseDirectory);
-        asmLoader.LoadFromPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "PaprikaTaskExample"));
+
+        if (locations != null && locations.Any())
+            foreach (String location in locations)
+                asmLoader.LoadFromPath(location);
+    }
+
+    static IList<String> GetAssemblyLocations(String paprikaConfigPath) {
+        XmlDocument config = XmlExt.LoadFromPath(paprikaConfigPath);
+        XmlNode locationsConfig = config.SelectSingleNode("configuration/locations");
+        if (locationsConfig != null) {
+            IList<String> locations = locationsConfig
+                .SelectNodes("location")
+                .Cast<XmlNode>()
+                .Select(config => {
+                    String pathValue = config.GetAttributeValue("path");
+                    if (String.IsNullOrEmpty(pathValue))
+                        throw new BadConfigurationException("Wrong Path configuration. '{pathValue}'.");
+
+                    return pathValue.Resolve();
+                })
+                .ToList();
+
+            return locations;
+        }
+
+        return Enumerable
+            .Empty<String>()
+            .ToList();
     }
 }
